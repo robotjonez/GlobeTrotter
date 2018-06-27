166847e39745f231ec7cd1262200c3b10df65f3a
robotjonez <curtisjonesofficial@gmail.com>
2018-06-26 19:02:31 -0500

    Added an GET endpoint in the JS file that passes in the submit value to test out the rest countries API name to see what kind of data gets passed back.

--- search_function.js
+++ search_function.js
@@ -2,4 +2,14 @@ function search_country()
 {
 	var country_name = document.getElementById("country_name").value;
 	console.log(country_name);
+
+  var request = new XMLHttpRequest();
+
+  request.open('GET', 'https://restcountries.eu/rest/v2/name/' + country_name, true);
+
+  request.onload = function () {
+    var data = JSON.parse(this.response);
+    console.log(data);
+  }
+  request.send();
 }
