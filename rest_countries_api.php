974a265702d8d9e839109d6fe99fddf539da6726
robotjonez <curtisjonesofficial@gmail.com>
2018-06-27 15:39:45 -0500

    Needed to move these variable out side of my function in order for it to work properly.

--- rest_countries_api.php
+++ rest_countries_api.php
@@ -1,8 +1,10 @@
 <?php
+
+	$function = $_REQUEST["function"];
+  $function();
+
   function get_countries_data_and_build_table()
   {
-    $function = $_REQUEST["function"];
-    $function();
     $country_name = $_REQUEST["country_name"];
     $request = file_get_contents('https://restcountries.eu/rest/v2/name/' . $country_name);
     $response = json_decode($request);
