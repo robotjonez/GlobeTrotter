7b4513709ad85708205df8ba249baa6f7623b0e7
robotjonez <curtisjonesofficial@gmail.com>
2018-06-27 13:53:57 -0500

    Added CSS to my application, added a div and ID where my future table will live. Added some error handling to my Javascript http request and added a temporary way to display the response untill I hook up php to actually build the table and the api call.

--- search_function.js
+++ search_function.js
@@ -1,15 +1,16 @@
 function search_country()
 {
-	var country_name = document.getElementById("country_name").value;
-	console.log(country_name);
+	var country_name = document.getElementById("search").value;
 
-  var request = new XMLHttpRequest();
-
-  request.open('GET', 'https://restcountries.eu/rest/v2/name/' + country_name, true);
-
-  request.onload = function () {
-    var data = JSON.parse(this.response);
-    console.log(data);
+  if (country_name != "")
+	{
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://restcountries.eu/rest/v2/name/' + country_name, true);
+    request.send();
+    document.getElementById("countries_table_display").innerHTML = request.responseText;
   }
-  request.send();
+  else
+  {
+    window.alert("Search Field Is Empty, You Must Search a country by its name.");
+  }
 }
