82f8230dd42ba70afbc215f2538699df4125ea41
robotjonez <curtisjonesofficial@gmail.com>
2018-06-27 20:54:11 -0500

    Added table data and for loops for accessing the nested data objects. added styling for table data.

--- rest_countries_api.php
+++ rest_countries_api.php
@@ -7,7 +7,7 @@
   {
     $search_value = $_REQUEST["search_value"];
 
-    if ($search_value != null && strlen($search_value) > 3)
+    if ($search_value != null && strlen($search_value) > 2)
     {
       $request = file_get_contents('https://restcountries.eu/rest/v2/name/' . $search_value);
       $response = json_decode($request);
@@ -17,8 +17,8 @@
       $response = json_decode($request);
     }
 
-		if ($response != null)
-		{
+		if ($response != null && $response != "")
+    {
 			echo "<table>";
 				echo "<tr>";
 					echo "<th>Country Name</th>";
@@ -30,9 +30,37 @@
 					echo "<th>Population</th>";
 					echo "<th>Language(s)</th>";
 				echo "</tr>";
-      echo "</table>";
-      var_dump($response);
-    }
-  }
 
+			for ($i = 0; $response[$i] < 50; $i++)
+			{
+				echo "<tr>";
+					echo "<td>" . $response[$i]->name . "</td>";
+					echo "<td>" . $response[$i]->alpha2Code . "</td>";
+					echo "<td>" . $response[$i]->alpha3Code . "</td>";
+					echo "<td><img src='" . $response[$i]->flag . "'</td>";
+					echo "<td>" . $response[$i]->region . "</td>";
+					echo "<td>" . $response[$i]->subregion . "</td>";
+					echo "<td>" . number_format($response[$i]->population) . "</td>";
+					echo "<td>";
+						for ($key = 0; $key < count($response[$i]->languages); $key++)
+						{
+							if (count($response[$i]->languages) > 1)
+							{
+								echo $response[$i]->languages[$key]->name . "<br>";
+							}
+							else
+							{
+								echo $response[$i]->languages[$key]->name;
+							}
+						}
+					echo "</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+		}
+		else
+		{
+			echo "error";
+		}
+	}
 ?>
